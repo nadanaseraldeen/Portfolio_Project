1c9da9fbf9c633f43079a4130ff145fc1f639243
nadanaseraldeen <201081@ppu.edu.ps>
2024-04-15 04:06:08 +0300

    Website responsive using grid bootstrap

--- Portfolio Project/portfolio.js
+++ Portfolio Project/portfolio.js
@@ -1,34 +1,28 @@
-var aboutLinks = document.getElementsByClassName('aboutLinks');
-var linksContent = document.getElementsByClassName('linksContent');
+var aboutLinks = document.getElementsByClassName("aboutLinks");
+var linksContent = document.getElementsByClassName("linksContent");
 
-function openTab(tabname){
-    for(aboutLink of aboutLinks)
-    {
-        aboutLink.classList.remove('active-link');
-    }
-    for(linksContents of linksContent)
-    {
-        linksContents.classList.remove('active-tab');
-    }
-    event.currentTarget.classList.add('active-link');
-    document.getElementById(tabname).classList.add('active-tab');
-
+function openTab(tabname) {
+  for (aboutLink of aboutLinks) {
+    aboutLink.classList.remove("active-link");
+  }
+  for (linksContents of linksContent) {
+    linksContents.classList.remove("active-tab");
+  }
+  event.currentTarget.classList.add("active-link");
+  document.getElementById(tabname).classList.add("active-tab");
 }
 
 const scrollToTopBtn = document.getElementById("scrollToTopBtn");
 
 window.onscroll = function () {
-    if (document.body.scrollTop > 25 || document.documentElement.scrollTop > 25) {
-        scrollToTopBtn.style.display = "block";
-    } else {
-        scrollToTopBtn.style.display = "none";
-    }
+  if (document.body.scrollTop > 25 || document.documentElement.scrollTop > 25) {
+    scrollToTopBtn.style.display = "block";
+  } else {
+    scrollToTopBtn.style.display = "none";
+  }
 };
 
 scrollToTopBtn.onclick = function () {
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
 };
-
-  
-  
